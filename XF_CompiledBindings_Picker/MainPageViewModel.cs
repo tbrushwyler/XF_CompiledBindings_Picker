0d44faf563efeaca27ddee9d8bc6a0323d9d366b
Taylor Brushwyler <taylor.brushwyler@toro.com>
2019-02-13 13:04:29 -0500

    Initial commit

---
+++ XF_CompiledBindings_Picker/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace XF_CompiledBindings_Picker
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<PickerOption> Options { get; private set; }
+
+        public MainPageViewModel()
+        {
+            Options = new ObservableCollection<PickerOption>()
+            {
+                new PickerOption(1, "First"),
+                new PickerOption(2, "Second"),
+            };
+        }
+    }
+
+    public class PickerOption
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public PickerOption(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
